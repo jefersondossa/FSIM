1c35d7c7fe6d0744fa005ab23cb46775383e399b
jefersondossa <jefersondossa@usp.br>
2018-05-09 08:40:39 -0300

    documented

---
+++ paraview.py
@@ -0,0 +1,11 @@
+RenderView1 = GetRenderView() 
+
+# Turn off "Head Light" 
+RenderView1.LightSwitch = 0 
+
+# Turn off "Light Kit" 
+RenderView1.UseLight = 0 
+
+r = GetDisplayProperties() 
+r.Ambient = 1.0 
+r.Diffuse = 0.0
