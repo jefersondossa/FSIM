00baf8b135ee432ce6d13f8888108d484863f300
jefersondossa <jefersondossa@usp.br>
2018-05-16 17:19:36 -0300

    a

--- IntegrationQuadrature11.hpp
+++ IntegrationQuadrature11.hpp
@@ -33,6 +33,9 @@ public:
     //Integration point logical vector
     typedef ublas::bounded_vector<bool, 4*DIM-1>        PointLogical;
 
+    /// integration points int variable
+    typedef ublas::bounded_vector<int, 4*DIM-1>         PointIntVar;
+
     //Defines the numerical integration iterator
     typedef typename PointWeight::iterator              QuadratureListIt;
 
