1745d6ee4e109267eaf952bba70add9231bf2f7b
jefersondossa <jwdfernandes@gmail.com>
2024-01-30 20:15:26 -0300

    Including classic linear frame structures

--- WeakForm/Elasticity/LinearBeam.cpp
+++ WeakForm/Elasticity/LinearBeam.cpp
@@ -10,97 +10,97 @@ LinearBeam::LinearBeam(int matid, int dim, double young, double inertia) : WeakF
 };
 
 
-void LinearFrame::ComputeStiffness(int &index, IntPointData &data, MatrixDouble &Stiffness){
-    if (!data.fNeedsDSol || !data.fNeedsSol){
-        data.fNeedsDSol = true;
-        data.fDSolDx.resize(fNState,fDimension);
-        data.fNeedsSol = true;
-        data.fSol.resize(fNState);
-    }
+void LinearBeam::ComputeStiffness(int &index, IntPointData &data, MatrixDouble &Stiffness){
+    // if (!data.fNeedsDSol || !data.fNeedsSol){
+    //     data.fNeedsDSol = true;
+    //     data.fDSolDx.resize(fNState,fDimension);
+    //     data.fNeedsSol = true;
+    //     data.fSol.resize(fNState);
+    // }
     
-    double WJ = data.fWeight * data.fJacA0 * data.fWeightFunction[index];
-    int nphi = data.fPhi.size();
-    double elementLenght = 2.*data.fJacA0;
-    double K = fYoungModulus * fArea / elementLenght;
+    // double WJ = data.fWeight * data.fJacA0 * data.fWeightFunction[index];
+    // int nphi = data.fPhi.size();
+    // double elementLenght = 2.*data.fJacA0;
+    // double K = fYoungModulus * fArea / elementLenght;
 
 
-    MatrixDouble rotation(fDimension*nphi,fDimension*nphi);
-    MatrixDouble matB(fDimension,fDimension*nphi);
-    rotation.setZero();
-    matB.setZero();
-    double cosa = data.fAxes0(0,0) / data.fJacA0;
-    double sina = data.fAxes0(1,0) / data.fJacA0;
-    double check = sina*sina+cosa*cosa;
-    for (int j = 0; j < nphi; j++){
-        // for (int i = 0; i < fDimension; i++){
-            matB(0,fDimension*j) = data.fDPhiX0(j,0);
-        // }
-        rotation(2*j  ,2*j  ) = cosa;
-        rotation(2*j+1,2*j  ) = sina;
-        rotation(2*j  ,2*j+1) = -sina;
-        rotation(2*j+1,2*j+1) = cosa;
-    }
-    // std::cout << "matB =\n"<< matB << std::endl;
-    // std::cout << "rotation =\n"<< rotation << std::endl;
-    // std::cout << "K =\n"<< matB.transpose() * matB << std::endl;
-    Stiffness += rotation * matB.transpose() * matB * rotation.transpose() * WJ * elementLenght * K;
+    // MatrixDouble rotation(fDimension*nphi,fDimension*nphi);
+    // MatrixDouble matB(fDimension,fDimension*nphi);
+    // rotation.setZero();
+    // matB.setZero();
+    // double cosa = data.fAxes0(0,0) / data.fJacA0;
+    // double sina = data.fAxes0(1,0) / data.fJacA0;
+    // double check = sina*sina+cosa*cosa;
+    // for (int j = 0; j < nphi; j++){
+    //     // for (int i = 0; i < fDimension; i++){
+    //         matB(0,fDimension*j) = data.fDPhiX0(j,0);
+    //     // }
+    //     rotation(2*j  ,2*j  ) = cosa;
+    //     rotation(2*j+1,2*j  ) = sina;
+    //     rotation(2*j  ,2*j+1) = -sina;
+    //     rotation(2*j+1,2*j+1) = cosa;
+    // }
+    // // std::cout << "matB =\n"<< matB << std::endl;
+    // // std::cout << "rotation =\n"<< rotation << std::endl;
+    // // std::cout << "K =\n"<< matB.transpose() * matB << std::endl;
+    // Stiffness += rotation * matB.transpose() * matB * rotation.transpose() * WJ * elementLenght * K;
 
 }
 
-void LinearFrame::ComputeResidual(int &index, IntPointData &data, VecDouble &Rhs){
+void LinearBeam::ComputeResidual(int &index, IntPointData &data, VecDouble &Rhs){
 
-    int nphi = data.fPhi.size();
-    double WJ = data.fWeight * data.fJacA0 * data.fWeightFunction[index];
-    auto force = fForceFunction;
-    VecDouble forcingF(fDimension);
-    VecDouble x_ = data.fX;
-    if (force) force(x_,forcingF);
+    // int nphi = data.fPhi.size();
+    // double WJ = data.fWeight * data.fJacA0 * data.fWeightFunction[index];
+    // auto force = fForceFunction;
+    // VecDouble forcingF(fDimension);
+    // VecDouble x_ = data.fX;
+    // if (force) force(x_,forcingF);
     
-    double elementLenght = 2.*data.fJacA0;
-    double K = fYoungModulus * fArea / elementLenght;
+    // double elementLenght = 2.*data.fJacA0;
+    // double K = fYoungModulus * fArea / elementLenght;
 
-    MatrixDouble rotation(fDimension*nphi,fDimension*nphi);
-    MatrixDouble matB(fDimension,fDimension*nphi);
-    rotation.setZero();
-    matB.setZero();
-    double cosa = data.fAxes0(0,0) / data.fJacA0;
-    double sina = data.fAxes0(1,0) / data.fJacA0;
-    double check = sina*sina+cosa*cosa;
-    for (int j = 0; j < nphi; j++){
-        // for (int i = 0; i < fDimension; i++){
-            matB(0,fDimension*j) = data.fDPhiX0(j,0);
-        // }
-        rotation(2*j  ,2*j  ) = cosa;
-        rotation(2*j+1,2*j  ) = sina;
-        rotation(2*j  ,2*j+1) = -sina;
-        rotation(2*j+1,2*j+1) = cosa;
-    }
+    // MatrixDouble rotation(fDimension*nphi,fDimension*nphi);
+    // MatrixDouble matB(fDimension,fDimension*nphi);
+    // rotation.setZero();
+    // matB.setZero();
+    // double cosa = data.fAxes0(0,0) / data.fJacA0;
+    // double sina = data.fAxes0(1,0) / data.fJacA0;
+    // double check = sina*sina+cosa*cosa;
+    // for (int j = 0; j < nphi; j++){
+    //     // for (int i = 0; i < fDimension; i++){
+    //         matB(0,fDimension*j) = data.fDPhiX0(j,0);
+    //     // }
+    //     rotation(2*j  ,2*j  ) = cosa;
+    //     rotation(2*j+1,2*j  ) = sina;
+    //     rotation(2*j  ,2*j+1) = -sina;
+    //     rotation(2*j+1,2*j+1) = cosa;
+    // }
 
-    VecDouble sol(2);// = data.fSol;
-    sol[0] = -data.fDSolDx(1,0)*sina;
-    sol[1] = +data.fDSolDx(0,0)*cosa;
+    // VecDouble sol(2);// = data.fSol;
+    // sol[0] = -data.fDSolDx(1,0)*sina;
+    // sol[1] = +data.fDSolDx(0,0)*cosa;
 
-    // std::cout << "rotation =\n"<< rotation << std::endl;
-    Rhs += rotation * matB.transpose() * sol * WJ * elementLenght * K;
+    // // std::cout << "rotation =\n"<< rotation << std::endl;
+    // Rhs += rotation * matB.transpose() * sol * WJ * elementLenght * K;
 
-    for (int i = nphi; i--; ){
-        double shapeFi = data.fPhi[i];
-        //External force
-        double Fx = forcingF[0] * shapeFi;
-        double Fy = forcingF[1] * shapeFi;
-        Rhs[2*i  ] += Fx * WJ;
-        Rhs[2*i+1] += Fy * WJ;
-    };
-    // std::cout << "RHS = " << Rhs << std::endl;
+    // for (int i = nphi; i--; ){
+    //     double shapeFi = data.fPhi[i];
+    //     //External force
+    //     double Fx = forcingF[0] * shapeFi;
+    //     double Fy = forcingF[1] * shapeFi;
+    //     Rhs[2*i  ] += Fx * WJ;
+    //     Rhs[2*i+1] += Fy * WJ;
+    // };
+    // // std::cout << "RHS = " << Rhs << std::endl;
     
 };
 
-void LinearFrame::ComputeError(IntPointData &data, VecDouble &errors){
+void LinearBeam::ComputeError(IntPointData &data, VecDouble &errors){
     std::cout << "Not implemented yet\n";
     PanicButton();
 }
 
-int LinearFrame::VariableIndex(const std::string &name) const{
+int LinearBeam::VariableIndex(const std::string &name) const{
     
     if(!strcmp("Displacement",name.c_str()))           return 1;
     if(!strcmp("Rotation",name.c_str()))               return 2;
@@ -114,7 +114,7 @@ int LinearFrame::VariableIndex(const std::string &name) const{
     return -1;
 };
 
-int LinearFrame::NSolutionVariables(int var) const{
+int LinearBeam::NSolutionVariables(int var) const{
     switch (var)
     {
     case 1:
@@ -131,53 +131,53 @@ int LinearFrame::NSolutionVariables(int var) const{
     }
 };
 
-void LinearFrame::Solution(IntPointData &data, int var, VecDouble &Sol) {
+void LinearBeam::Solution(IntPointData &data, int var, VecDouble &Sol) {
 
-    //Displacement
-    if (var == 1){
-        Sol[0] = data.fSol[0];
-        Sol[1] = data.fSol[1];
-        Sol[2] = 0.;
-        return;
-    };
+    // //Displacement
+    // if (var == 1){
+    //     Sol[0] = data.fSol[0];
+    //     Sol[1] = data.fSol[1];
+    //     Sol[2] = 0.;
+    //     return;
+    // };
 
-    //NormalStress
-    if (var == 2){
-        double cosa = data.fAxes0(0,0) / data.fJacA0;
-        double sina = data.fAxes0(1,0) / data.fJacA0;
-        Sol[0] = fYoungModulus * (data.fDSolDx(1,0)*sina - data.fDSolDx(0,0)*cosa) ;
-        return;
-    };
+    // //NormalStress
+    // if (var == 2){
+    //     double cosa = data.fAxes0(0,0) / data.fJacA0;
+    //     double sina = data.fAxes0(1,0) / data.fJacA0;
+    //     Sol[0] = fYoungModulus * (data.fDSolDx(1,0)*sina - data.fDSolDx(0,0)*cosa) ;
+    //     return;
+    // };
 
-    VecDouble forcingF(fDimension);
-    VecDouble x_ = data.fX;
-    if (fForceFunction) fForceFunction(x_,forcingF);
+    // VecDouble forcingF(fDimension);
+    // VecDouble x_ = data.fX;
+    // if (fForceFunction) fForceFunction(x_,forcingF);
 
-    VecDouble disp(fDimension);
-    MatrixDouble gradDisp(fDimension,3);
-    if (fExactSol) fExactSol(x_,disp,gradDisp);
+    // VecDouble disp(fDimension);
+    // MatrixDouble gradDisp(fDimension,3);
+    // if (fExactSol) fExactSol(x_,disp,gradDisp);
 
-    //Exact Displacement
-    if (var == 3){
-        Sol[0] = disp[0];
-        Sol[1] = disp[1];
-        Sol[2] = 0.;
-        return;
-    };
+    // //Exact Displacement
+    // if (var == 3){
+    //     Sol[0] = disp[0];
+    //     Sol[1] = disp[1];
+    //     Sol[2] = 0.;
+    //     return;
+    // };
     
-    //Exact Sigma X
-    if (var == 4){
-        double epsilon = gradDisp.norm();
-        Sol[0] = fYoungModulus * epsilon;
-        return;
-    };
+    // //Exact Sigma X
+    // if (var == 4){
+    //     double epsilon = gradDisp.norm();
+    //     Sol[0] = fYoungModulus * epsilon;
+    //     return;
+    // };
 
-    //Exact Force
-    if (var == 5){
-        Sol[0] = forcingF[0];
-        Sol[1] = forcingF[1];
-        Sol[2] = 0.;
-        return;
-    };
+    // //Exact Force
+    // if (var == 5){
+    //     Sol[0] = forcingF[0];
+    //     Sol[1] = forcingF[1];
+    //     Sol[2] = 0.;
+    //     return;
+    // };
 
 }; 
