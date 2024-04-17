0472716d413e87fcc949c9c0d75d2f07b6dd9ffa
felipinho2003 <felipinho.asc@gmail.com>
2024-04-17 09:10:28 -0300

    start implementing graphic interface

---
+++ Window.cpp
@@ -0,0 +1,12 @@
+// Graphical Interface (GUI).cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include "WindowConstructor.h"
+
+
+int main()
+{
+    WindowConstructor c;
+    Fl::run();
+    int a =0;
+}
--- examples/clamped_beam1.h
+++ examples/clamped_beam1.h
@@ -1,4 +1,7 @@
 
+   //Verify yhe main.cpp
+   //Verify path in .vscode\launch.json
+
    // Defines the problem dimension
     const int dimension = 2;
 {
@@ -14,56 +17,53 @@ auto yieldFunction = [](const double &plast, double &sigma_y, double &hardening)
 
     CompMesh* cmesh = new CompMesh(); 
 
-    Elasticity2D * matelas = new Elasticity2D(10,1.e3,.0,false);
+    Elasticity2D * matelas = new Elasticity2D(13,1.e3,.3,false);
     // ElasticityPositional2D * matelas = new ElasticityPositional2D(10,1.e3,.0,false);
 
-    //BC
+    //Top
     MatrixDouble val1(2,2);
     val1.setZero();
     VecDouble val2(2);
     val2.setZero();
+    L2Projection * matbc1 = new L2Projection(14,2,0,val1,val2);
+
+    
     //Left
-    val2[1] = 0.;
-    L2Projection * matbc1 = new L2Projection(9,2,0,val1,val2);
-
     val2.setZero();
-    val2[0] = 0.;
-    // //bottom
-    // L2Projection * matbc2 = new L2Projection(11,2,1,val1,val2);
 
-    val2.setZero();
-    // val2[1] = 0.0005;
-    // val2[1] = 0.0;
+    val2[1] = -0.06;
+    val2[0] = 0.02;
+    L2Projection * matbc2 = new L2Projection(11,2,0,val1,val2);
+
     //Right
-    L2Projection * matbc3 = new L2Projection(12,2,0,val1,val2);
     val2.setZero();
-    L2Projection * matbc4 = new L2Projection(8,2,1,val1,val2);
-    // val2[0] = 1.;
-    L2Projection * matbc5 = new L2Projection(13,2,1,val1,val2);
+
+    val2[0]=-0.06;
+    val2[1]=0.07;
+
+    L2Projection * matbc3 = new L2Projection(12,2,0,val1,val2);
 
     cmesh->InsertMaterial(matbc1);
-    // cmesh->InsertMaterial(matbc2);
+    cmesh->InsertMaterial(matbc2);
     cmesh->InsertMaterial(matbc3);
-    cmesh->InsertMaterial(matbc4);
-    cmesh->InsertMaterial(matbc5);
-
 
     // VonMises *plastmodel = new VonMises(matelas);
     // Tresca *plastmodel = new Tresca(matelas);
-    double angle = 45.*M_PI/180.;
-    DruckerPrager *plastmodel = new DruckerPrager(matelas,angle,angle,false);
-    plastmodel->SetUniaxialYieldFunction(yieldFunction);
-    // cmesh->InsertMaterial(matelas);
-    cmesh->InsertMaterial(plastmodel); 
+    //double angle = 45.*M_PI/180.;
+    //DruckerPrager *plastmodel = new DruckerPrager(matelas,angle,angle,false);
+    //plastmodel->SetUniaxialYieldFunction(yieldFunction);
+    cmesh->InsertMaterial(matelas);
+    //cmesh->InsertMaterial(plastmodel); 
     std::vector<L2Projection *> bcIncrement = {matbc3};
    
-    GmshTools::Read(*cmesh,"../marques_viga_biengastada.msh");
+    GmshTools::Read(*cmesh,"/home/felipe_costa_ubuntu/IC2023/Atividades Felipe/Rosto_Bolacha/Rosto_bolacha.msh");
 
-    IncrementalAnalysis an(cmesh,SolverType::ELU, 100, bcIncrement,1.e-7,30);
+    IncrementalAnalysis an(cmesh,SolverType::ELU, 1, bcIncrement,1.e-7,30);
     VecDouble increment(2);
     increment.setZero();    
-    increment[1] = 0.000035;
-    // increment[1] = 0.02;
+    //increment[1] = 0.000035;
+    increment[1] = 0.02;
+    //Quanto de encremento
     an.SetIncrement(increment);
 #ifdef RELEASE_BUILD
     an.SType() = SolverType::EUmfpack;
@@ -71,8 +71,8 @@ auto yieldFunction = [](const double &plast, double &sigma_y, double &hardening)
 #endif
 
     std::vector<std::string> ScalarNames, VectorNames;
-    ScalarNames = {"PlasticStrain"};
-    VectorNames = {"Displacement","Stress","Strain","RealStress"};
+    //ScalarNames = {"PlasticStrain"};
+    VectorNames = {"Displacement","Stress","Strain"};
     an.Run("plasticitytest",ScalarNames,VectorNames);
     
     // std::vector<std::string> integrate = {"Solution","DerivativeX","DerivativeY"};
