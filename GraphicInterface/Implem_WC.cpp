bc3e4a55c6287dbd97f5cb5435ce26da975775f2
jefersondossa <jwdfernandes@gmail.com>
2024-04-22 20:09:20 -0300

    Update graph interface option

--- GraphicInterface/Implem_WC.cpp
+++ GraphicInterface/Implem_WC.cpp
@@ -1,7 +1,12 @@
 #include "WindowConstructor.h"
+
+#ifdef HAS_GRAPHINTERFACE
 #include <FL/Fl_File_Chooser.H>
 //Delete Window->user_data((void*)(this)) in cpp file
 //Add #include <FL/Fl_File_Chooser.H> in .h file
 //Add #include <iostream> in .h file
 //See after the tree tool
 
+
+
+#endif
