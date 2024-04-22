93b1edd197b36e6bda1128f70609679280995830
felipinho2003 <felipinho.asc@gmail.com>
2024-04-22 10:15:06 -0300

    implementing graphic interface

---
+++ GraphicInterface/Implem_WC.cpp
@@ -0,0 +1,7 @@
+#include "WindowConstructor.h"
+#include <FL/Fl_File_Chooser.H>
+//Delete Window->user_data((void*)(this)) in cpp file
+//Add #include <FL/Fl_File_Chooser.H> in .h file
+//Add #include <iostream> in .h file
+//See after the tree tool
+
