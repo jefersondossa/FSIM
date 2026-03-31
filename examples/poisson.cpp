0c81b92df228c5b5ddafd316ac37932d4f89de60
jefersonUFMG <jwdfernandes@gmail.com>
2026-03-31 14:23:14 -0300

    update poisson example

--- examples/poisson.cpp
+++ examples/poisson.cpp
@@ -1,4 +1,10 @@
-
+#include "GeoMesh.h"
+#include "LinearAnalysis.h"
+#include "MixedCompMesh.h"
+#include "GmshTools.h"
+#include "VTUGenerator.h"
+#include "L2Projection.h"
+#include "Poisson.h"
 
 auto exactSol = [](const VecDouble &coord, VecDouble &u, MatrixDouble &gradU){
     const auto &x=coord[0];
@@ -17,14 +23,14 @@ auto forcing = [](const VecDouble &coord, VecDouble &force){
     force[0] = -1;
 };
 
-{
+int main(int argc, char **args) { 
 
     //Geometric Mesh
     GeoMesh * gmesh = new GeoMesh();
     GmshTools::Read(*gmesh,"../hierarquic2d.msh");
     gmesh->Print("gmesh.txt");
 
-    CompMesh* cmesh = new CompMesh(ApproxType::EIsoparametric);
+    CompMesh* cmesh = new CompMesh(gmesh,ApproxType::EIsoparametric);
 
 
     Poisson * matelas = new Poisson(8,1,1);
@@ -32,20 +38,17 @@ auto forcing = [](const VecDouble &coord, VecDouble &force){
     matelas->SetExactSolution(exactSol);
     cmesh->InsertMaterial(matelas);
 
-
     //BC
     MatrixDouble val1(1,1);
     val1.setZero();
     VecDouble val2(1);
     val2.setZero();
-    //Left
-    val2[0] = 0.;
     L2Projection * matbc1 = new L2Projection(5,0,BoundaryConditionType::kDirichlet,val1,val2);
-
-    val2[0] = 0.;
-    //Right
     L2Projection * matbc2 = new L2Projection(6,0,BoundaryConditionType::kDirichlet,val1,val2);
     L2Projection * matbc3 = new L2Projection(7,0,BoundaryConditionType::kDirichlet,val1,val2);
+    matbc1->SetExactSolution(exactSol);
+    matbc2->SetExactSolution(exactSol);
+    matbc3->SetExactSolution(exactSol);
 
     cmesh->InsertMaterial(matbc1);
     cmesh->InsertMaterial(matbc2);
@@ -59,13 +62,13 @@ auto forcing = [](const VecDouble &coord, VecDouble &force){
    
     std::vector<std::string> ScalarNames, VectorNames;
     // VectorNames = {"Solution"};
-    ScalarNames = {"Solution"};
+    ScalarNames = {"Solution","ExactSolution"};
     an.Run();
-    cmesh->Print("cmeshup.txt");
+
 
     VecDouble errors(3);
     an.PostProcessError(errors);
 
-    VTUGenerator::PrintResults(cmesh,"resultbeam",ScalarNames,VectorNames);    
+    VTUGenerator::PrintResults(cmesh,"poisson",ScalarNames,VectorNames);    
         
 }           
